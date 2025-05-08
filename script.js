d2e7de43fd13de0057e0b72927e7f564cb4c183c
Sung Hun Jung <codingj87@zensystems.co.kr>
2025-05-08 10:31:53 +0900

    초기 커밋

---
+++ script.js
@@ -0,0 +1,11 @@
+function addTodo() {
+  const input = document.getElementById("todo-input");
+  const list = document.getElementById("todo-list");
+
+  if (input.value.trim() !== "") {
+    const li = document.createElement("li");
+    li.textContent = input.value;
+    list.appendChild(li);
+    input.value = "";
+  }
+}
